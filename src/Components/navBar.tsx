6e80875163d9164b34c9de0c278121ec0f210438
Lucas Bispo <skaliboo1@gmail.com>
2022-09-14 21:44:06 -0300

    fix: resolve um bug no css

--- src/Components/navBar.tsx
+++ src/Components/navBar.tsx
@@ -2,19 +2,23 @@ import styled from "styled-components";
 
 function NavBar() {
     const Header = styled.header`
-    background-color: #0A192F;
-    backdrop-filter: blur( 13.5px );
+        align-items: center;
+        background-color: #0a192f;
+        backdrop-filter: blur(13.5px);
         width: 100%;
-        height: 70px;
+        height: 80px;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         h1 {
             margin-left: 40px;
         }
+        @media (max-width: 800px) {
+            height: 100px;
+        }
     `;
     const Ul = styled.ul`
-    list-style: none;
+        list-style: none;
         display: flex;
         flex-direction: row;
         margin-right: 40px;
@@ -22,17 +26,28 @@ function NavBar() {
         align-items: center;
         width: 591px;
         height: 40px;
-        color: #8892B0;
-        span{
-        color: #64FBDA;}
+        color: #8892b0;
+        opacity: 1;
+        @media (max-width: 800px) {
+            opacity: 0;
+        }
+        span {
+            color: #64fbda;
+        }
     `;
     return (
         <Header>
             <h1>logo</h1>
             <Ul>
-                <li><span>1.</span>sobre</li>
-                <li><span>2.</span>projetos</li>
-                <li><span>3.</span>contato</li>
+                <li>
+                    <span>1.</span>sobre
+                </li>
+                <li>
+                    <span>2.</span>projetos
+                </li>
+                <li>
+                    <span>3.</span>contato
+                </li>
                 <button>Dowloads meu curriculo</button>
             </Ul>
         </Header>
